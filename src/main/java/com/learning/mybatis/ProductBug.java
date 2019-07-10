a7eec3ab0fb230203aba30cc98cf36c46875e345
xiaomeng1 <meng_xiangli@163.com>
2019-07-11 07:54:34 +0800

    test bug

---
+++ src/main/java/com/learning/mybatis/ProductBug.java
@@ -0,0 +1,14 @@
+package com.learning.mybatis;
+
+/**
+ * Created by mengxiangli on 2019/7/11.
+ */
+public class ProductBug {
+
+    public static void main(String[] args) {
+        System.out.println("1111");
+        System.out.println("1111");
+        System.out.println("2222");
+        System.out.println("2222");
+    }
+}
